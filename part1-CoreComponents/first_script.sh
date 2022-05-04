0905dfc9a4595c756266e8b8e865b482a63bd254
Igor Tomazelli Nietmann <Igor.Tomazelli.Nietmann@ibm.com>
2022-05-04 18:08:23 -0300

    Making a script look professional

--- part1-CoreComponents/first_script.sh
+++ part1-CoreComponents/first_script.sh
@@ -2,5 +2,16 @@
 #the first line is called 'hash-bang' line, and is used to tell to the OS how it should interpret the file (as a BASH script or as a python script)
 #the path to the interpreter is given right after the hash-bang 
 
+# Author: Igor T. Nietmann
+# Date Created: 04/05/2022
+# Last Modified: 04/05/2022
+
+# Description
+# Prints "This is my first script" to the terminal
+
+# Usage
+# first_script
+
+#Prints text to screen
 echo "This is my first script"
 exit 0 #zero is the exit-code standard for ending without errors. Any other number can represent an error
