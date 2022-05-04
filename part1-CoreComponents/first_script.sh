ff5fdd6ed02ebcd676a006e1092cdf657a40f23a
Igor Tomazelli Nietmann <Igor.Tomazelli.Nietmann@ibm.com>
2022-05-04 18:01:56 -0300

    First script

---
+++ part1-CoreComponents/first_script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#the first line is called 'hash-bang' line, and is used to tell to the OS how it should interpret the file (as a BASH script or as a python script)
+#the path to the interpreter is given right after the hash-bang 
+
+echo "This is my first script"
+exit 0 #zero is the exit-code standard for ending without errors. Any other number can represent an error
