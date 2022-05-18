1d76032be933ca31d7f30fc46e31aac2e7aa7da3
Igor Tomazelli Nietmann <Igor.Tomazelli.Nietmann@ibm.com>
2022-05-18 16:56:27 -0300

    Section 2 - Shell Variables

---
+++ part2-VariablesAndShellExpansion/shellVariableDemo.sh
@@ -0,0 +1,5 @@
+echo "Hello, $USER user"
+echo "the hostname variable is: $HOSTNAME"
+echo "the host type is: $HOSTTYPE"
+echo "the PS1 variable defines the style of the command prompt, and it is set to:"
+echo "${PS1}"
---
+++ part2-VariablesAndShellExpansion/variablePractice.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+student="Igor" #Can't use spaces to define variables
+echo "Hello ${student}"
